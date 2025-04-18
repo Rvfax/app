a40cadfaa3a78328ed46c2c1d2adf7a7840c140b
Rvfax <rafal.trzmielewski@gmail.com>
2025-04-18 02:00:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -25,3 +25,18 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
